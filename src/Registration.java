780682f18506c0db1bab53a62ef258646f1b637f
Sevdiyor <sevdiyoryoldoshev7@gmail.com>
2023-01-04 20:24:53 +0500

    Bu githubdagi ikkinchi ish

--- src/Registration.java
+++ src/Registration.java
@@ -1,6 +1,6 @@
 public class Registration {
     public static void main(String[] args) {
         System.out.println("Hello world");
-        System.out.println("Bir, bir");
+        System.out.println("Bir, bir, ikki");
     }
 }
